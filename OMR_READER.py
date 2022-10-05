8b9d772ec567115f9f98a7777982731c9d0411f0
ARUN <arunven3@gmail.com>
2022-10-05 14:14:41 +0530

    Changed functionalities for working based on dynamic inputs

--- OMR_READER.py
+++ OMR_READER.py
@@ -3,11 +3,11 @@ import numpy as np
 
 threasholdValue = 1000 # VALUE DETERMINE APPROXIMATE CIRCLE COLOURED VALUE
 pathImage = "C:/Users/venar/Downloads/newt1.jpg"
-heightImg = 700
-widthImg  = 700
 questions=10
 choices=4
-ans= [2,0,2,3,2,0,2,3,4,2]
+heightImg = questions * 100
+widthImg  = choices * 100
+ans= {1: 1, 2: 3, 3: 2, 4: 3, 5: 2, 6: 2, 7: 2, 8: 1, 9: 3, 10: 3}
 
 def reorder(myPoints):
     myPoints = myPoints.reshape((4, 2)) # REMOVE EXTRA BRACKET
@@ -53,16 +53,12 @@ imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
 imgCanny = cv2.Canny(imgBlur,10,70) # APPLY CANNY
 
 try:
-    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
-    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
     contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) # FIND ALL CONTOURS
-    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
     rectCon = rectContour(contours) # FILTER FOR RECTANGLE CONTOURS
     biggestPoints= getCornerPoints(rectCon[0]) # GET CORNER POINTS OF THE BIGGEST RECTANGLE
 
     if biggestPoints.size != 0 : # BIGGEST RECTANGLE WARPING
         biggestPoints=reorder(biggestPoints) # REORDER FOR WARPING
-        cv2.drawContours(imgBigContour, biggestPoints, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
         pts1 = np.float32(biggestPoints) # PREPARE POINTS FOR WARP
         pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
         matrix = cv2.getPerspectiveTransform(pts1, pts2) # GET TRANSFORMATION MATRIX
@@ -72,29 +68,33 @@ try:
         imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY) # CONVERT TO GRAYSCALE
         imgThresh = cv2.threshold(imgWarpGray, 170, 255,cv2.THRESH_BINARY_INV )[1] # APPLY THRESHOLD AND INVERSE
         boxes = splitBoxes(imgThresh, questions, choices) # GET INDIVIDUAL BOXES
-        countR=0
-        countC=0
+        countQuestions = 0
         boxIndex = 0
-        myIndex = []
+        choiceSelected = -1
+        answersMarked = {}
 
         for image in boxes:
             boxIndex +=1
             blackPixels = (np.sum(image == 255)) # FIND BLACK PIXELS INSIDE BOXES
+            marked = boxIndex % 4
 
             if blackPixels > threasholdValue : # COMPARE BLACK PIXELS WITH THRESHOLD VALUES
-                marked = boxIndex % 4
-                myIndex.append(4 if boxIndex % 4 == 0 else marked)
+                choiceSelected = 4 if boxIndex % 4 == 0 else marked
 
-        print("USER ANSWERS", myIndex)
+            if marked == 0 :
+                countQuestions += 1
+                answersMarked.update({countQuestions : choiceSelected})
+
+        print("USER ANSWERS", answersMarked)
         correctAnswers=0
 
-        for x in range(0, questions):
-             if ans[x] == myIndex[x]: # FIND CORRECT ANSWER
-                 correctAnswers +=1
+        for x, y in answersMarked.items():
+            if ans[x] == y :  # FIND CORRECT ANSWER
+                correctAnswers += 1
 
         score = (correctAnswers / questions) * 100  # FINAL GRADE
-        print("CORRECT ANSWERS", correctAnswers)
-        print("SCORE",score)
+        print("CORRECT ANSWERS : ", correctAnswers)
+        print("SCORE :", score)
 
 except Exception as e:
     print(str(e))
