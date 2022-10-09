fe2c0ac64d9b47fafcc1f6390befb929613decea
ARUN <arunven3@gmail.com>
2022-10-09 21:30:23 +0530

    Added functionality of storing datas to Excel sheet

--- OMR_READER.py
+++ OMR_READER.py
@@ -4,16 +4,15 @@ import numpy as np
 import time
 from pyzbar.pyzbar import decode
 from PIL import Image
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
-def barcodeReader(image):
-    # Decode the barcode image
+def barcodeReader(image): # Decode the barcode image
     detectedBarcodes = decode(image)
 
-    # If not detected then print the message
-    if not detectedBarcodes:
-        print("Barcode Not Detected or your barcode is blank/corrupted!")
+    if not detectedBarcodes: # If not detected then print the message
+        print("Error with this image, please try again...")
     else:
-        # Traverse through all the detected barcodes in image
         for barcode in detectedBarcodes:
             if barcode.data != "":
                 data = barcode.data.decode('utf-8')
@@ -47,27 +46,45 @@ def getCornerPoints(cont):
     approx = cv2.approxPolyDP(cont, 0.02 * peri, True) # APPROXIMATE THE POLY TO GET CORNER POINTS
     return approx
 
-def splitBoxes(img, questions, choices):
-    rows = np.vsplit(img, questions)
-    boxes=[]
-    for r in rows:
-        cols= np.hsplit(r,choices)
+def splitBoxes(img, questions, choices): # CREATE A SPECIFIC BOX FOR EACH CHOICES
+    rows = np.vsplit(img, questions) # SPLIT OMR IMAGE VERTICALLY BASED ON QUESTIONS
+    boxes={}
+    for i,r in enumerate(rows, start = 1):
+        cols= np.hsplit(r,choices) # SPLIT OMR IMAGE HORIZONTALLY BASED ON CHOICES
+        boxes[i] = []
         for box in cols:
-            boxes.append(box)
+            boxes[i].append(box) # APPEND CHOICES INTO EACH QUESTIONS
     return boxes
 
-start = time.time()
+start = time.time() # USED AS START TIME
 threasholdValue = 1000 # VALUE DETERMINE APPROXIMATE CIRCLE COLOURED VALUE
-pathImage = "Datas/*.jpg"
-ans= {1: 1, 2: 3, 3: 2, 4: 3, 5: 2, 6: 2, 7: 2, 8: 1, 9: 3, 10: 4}
+pathImage = "Datas/*.jpg" # OMR SHEET DATA PATH
+ans= {1: 1, 2: 3, 3: 2, 4: 3, 5: 2, 6: 2, 7: 2, 8: 1, 9: 3, 10: 4}  # ANSWER USED FOR VALIDATE
 path = glob.glob(pathImage)
 
-for imageData in path:
+for imageData in path: # ITTERATE OVER EACH IMAGE IN PATH
     img = cv2.imread(imageData)
-    barcodeData = barcodeReader(Image.open(imageData))
+    barcodeData = barcodeReader(Image.open(imageData)) # READ BARCODE DATAS
     questions = int(barcodeData[0])
     choices = int(barcodeData[1])
     regno = barcodeData[2]
+    answers = []
+    category = "Not_Categorized" if len(barcodeData) == 3 else barcodeData[3]
+    filename = "Results/" + category + ".xlsx" # EXCEL SHEET FILE PATH
+
+    try:
+        wb = load_workbook(filename)
+        ws = wb.worksheets[0]  # SELECT FIRST WORKSHEET
+    except FileNotFoundError:
+        headers_row = ['Name', 'Marks scored']
+        wb = Workbook()
+        ws = wb.active
+
+        for q in range(questions):
+            headers_row.append("Q" + repr(q + 1)) # MAKING HEADER ROW
+
+        ws.append(headers_row)
+
     heightImg = questions * 100
     widthImg = choices * 100
     img = cv2.resize(img, (widthImg, heightImg))  # RESIZE IMAGE
@@ -79,7 +96,6 @@ for imageData in path:
         contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # FIND ALL CONTOURS
         rectCon = rectContour(contours)  # FILTER FOR RECTANGLE CONTOURS
         biggestPoints = getCornerPoints(rectCon[0])  # GET CORNER POINTS OF THE BIGGEST RECTANGLE
-        # print(barcodeReader(getCornerPoints(rectCon[1])))
 
         if biggestPoints.size != 0:  # BIGGEST RECTANGLE WARPING
             biggestPoints = reorder(biggestPoints)  # REORDER FOR WARPING
@@ -90,24 +106,29 @@ for imageData in path:
 
             # APPLY THRESHOLD
             imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)  # CONVERT TO GRAYSCALE
+            # imgThresh =cv2.adaptiveThreshold(imgWarpGray, 225, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
             imgThresh = cv2.threshold(imgWarpGray, 170, 255, cv2.THRESH_BINARY_INV)[1]  # APPLY THRESHOLD AND INVERSE
             boxes = splitBoxes(imgThresh, questions, choices)  # GET INDIVIDUAL BOXES
-            countQuestions = 0
-            boxIndex = 0
-            choiceSelected = -1
+            choiceSelected = 0
             answersMarked = {}
 
-            for image in boxes:
-                boxIndex += 1
-                blackPixels = (np.sum(image == 255))  # FIND BLACK PIXELS INSIDE BOXES
-                marked = boxIndex % 4
+            for question, choice in boxes.items():
+                choiceSelected = -1
 
-                if blackPixels > threasholdValue: # COMPARE BLACK PIXELS WITH THRESHOLD VALUES
-                    choiceSelected = 4 if boxIndex % 4 == 0 else marked
+                for i,c in enumerate(choice, start = 1) :
+                    markedArea = (np.sum(c == 255))  # FIND WHITE PIXELS INSIDE BOXES
 
-                if marked == 0:
-                    countQuestions += 1
-                    answersMarked.update({countQuestions: choiceSelected})
+                    if markedArea > threasholdValue :  # COMPARE BLACK PIXELS WITH THRESHOLD VALUES
+                        choiceSelected = i if choiceSelected == -1 else -2
+
+                answersMarked.update({question: choiceSelected}) # MAPPING QUESTIONS AND ANSWERS
+
+                if choiceSelected > 0 :
+                    answers.append(choiceSelected)
+                elif choiceSelected == -1 :
+                    answers.append("Not answered")
+                elif choiceSelected == -2 :
+                    answers.append("More than one choice selected")
 
             print("USER ANSWERS", answersMarked)
             correctAnswers = 0
@@ -119,6 +140,8 @@ for imageData in path:
             score = (correctAnswers / questions) * 100  # FINAL GRADE
             print("CORRECT ANSWERS : ", correctAnswers)
             print("SCORE :", score)
+            ws.append([regno, correctAnswers] + answers) # APPEND STUDENT DATAS INTO EXCEL SHEET
+            wb.save(filename)
 
     except Exception as e:
         print(str(e))
